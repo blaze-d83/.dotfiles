7012a62eab2892bb61fabc486a738030a0e5aa7d
blaze-d83 <d.awareai83@gmail.com>
2025-09-22 12:12:04 +0530

    initial migrate and symlink

---
+++ nvim/lua/config/default.lua
@@ -0,0 +1,38 @@
+local shell = vim.fn.executable("bash") == 1 and "bash" or vim.o.shell
+
+-- Global
+vim.g.mapleader = " "
+vim.g.have_nerd_font = false
+
+-- Options
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.tabstop = 2
+vim.o.swapfile = false
+vim.o.signcolumn = "yes"
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.inccommand = "split"
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.scrolloff = 10
+vim.o.termguicolors = true
+vim.o.guicursor = "n-v-c-i-ci-ve-r-cr-o:block"
+vim.o.conceallevel = 1
+vim.opt.winborder = "rounded"
+
+-- Shell options
+vim.o.shell = shell
+vim.o.shellcmdflag = "-c"
+vim.o.shellquote = ""
+vim.o.shellredir = ">%s 2>&1"
+vim.o.shellpipe = "2>&1 | tee"
+vim.o.shellxquote = ""
+
+-- Other
+vim.o.autoindent = true
+vim.o.swapfile = false
+vim.wo.cursorline = true
+vim.opt.numberwidth = 5
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+-- Default configuration for themes
+local themes = {
+
+	-- Rose Pine theme
+	["rose-pine"] = {
+		"rose-pine/neovim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = false,
+				},
+			})
+			vim.cmd.colorscheme("rose-pine-main")
+		end,
+	},
+}
+
+local selected_theme = "rose-pine"
+
+if selected_theme == "" or selected_theme == "default" then
+	vim.cmd.colorscheme("habamax")
+	return {}
+else
+	return themes[selected_theme]
+end
