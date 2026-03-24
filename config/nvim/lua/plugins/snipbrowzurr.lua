d2bfed2864d5f34027f2ad58133784d98528b2dd
Darshan Aware <d.awareai83@gmail.com>
2026-03-24 11:31:54 +0530

    nvim: snipbrowzurr update

--- config/nvim/lua/plugins/snipbrowzurr.lua
+++ config/nvim/lua/plugins/snipbrowzurr.lua
@@ -2,6 +2,11 @@ return {
 	"blaze-d83/snipbrowzurr.nvim",
 	dependencies = { "L3MON4D3/LuaSnip", "ibhagwan/fzf-lua" },
 	config = function()
-		require("snipbrowzurr").setup()
+		require("snipbrowzurr").setup({
+          keymap = "<leader>sp",
+          load_vscode = true,
+          load_snipmate = true,
+          load_lua = true,
+        })
 	end,
 }
